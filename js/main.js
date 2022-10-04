67b8aa74316479d553453ea9040aaee0cf1930f5
ningsihapriyani <110323467+ningsihapriyani@users.noreply.github.com>
2022-10-04 22:21:16 +0700

    posisi
    posisi hero

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 gsap.from('.judul-hero', {duration:1.5, y:100, opacity:0});
+let tl =gsap.timeline(
+  {
+    scrollTrigger:{
+      trigger: 'dark',
+    }
+  }
+);
+
+tl.from('.wrap-arrum',{delay:.5, duration:1, y:100, opacity:0})
+  .from('.wanita', {delay:.5, duration:1, x:-100, opacity:0 })
+  .from('.pria', {duration:1, x:100, opacity:0 })
+  ;
